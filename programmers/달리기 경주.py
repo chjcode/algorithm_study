2110d174ac7b029ce9b391d3e98dee14f3a6ac29
chjcoder <chjcoder@gmail.com>
2024-03-27 12:51:24 +0900

    [python]programmers 달리기 경주

--- programmers/달리기 경주.py
+++ programmers/달리기 경주.py
@@ -1,28 +1,14 @@
-import java.util.*;
-
-class Solution {
-    public String[] solution(String[] players, String[] callings) {
+def solution(players, callings):
+    
+    dict = {}
+    for idx,player in enumerate(players):
+        dict[player] = idx
+    
+    for player in callings:
+        idx = dict[player]
+        dict[players[idx]] = idx-1
+        dict[players[idx-1]] = idx
         
-        // Map 만들기
-        Map<String, Integer> map = new HashMap<>();
-        // Map에 players 정보 저장
-        for (int i=0;i<players.length;i++){
-            map.put(players[i], i);
-        }
-        // 선수들 위치 바꾸기
-        for (String calling : callings){
-            int idx = map.get(calling);  // 현재 순위
-            
-            String temp = players[idx-1];   // 이전 index 선수 이름
-            players[idx-1] = calling;
-            players[idx] = temp;
-            
-            map.put(calling, idx-1);
-            map.put(temp, idx);
-        }
-        
-        
-        
-        return players;
-    }
-}
+        players[idx], players[idx-1] = players[idx-1], players[idx]
+    
+    return players
