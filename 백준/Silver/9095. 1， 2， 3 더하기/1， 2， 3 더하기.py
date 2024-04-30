04f7282202605e881035a51d2cd49771d7b13d12
chjcode <107929019+chjcode@users.noreply.github.com>
2024-04-30 09:14:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,20 @@
-def dp(n):
-  if n == 1:
-    return 1
-  elif n == 2:
-    return 2
-  elif n == 3:
-    return 4
-  else:
-    return dp(n-1) + dp(n-2) + dp(n-3)
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    lst = [0 for _ in range(n+1)]
 
-
-T = int(input())
-
-for _ in range(T):
-  n = int(input())
-  print(dp(n))
+    if n == 1:
+        print(1)
+    elif n == 2:
+        print(2)
+    elif n == 3:
+        print(4)
+    else:
+        lst[1] = 1
+        lst[2] = 2
+        lst[3] = 4
+        for i in range(4,n+1):
+            lst[i] = sum(lst[i-3:])
+        
+        # print(lst)
+        print(lst[n])
