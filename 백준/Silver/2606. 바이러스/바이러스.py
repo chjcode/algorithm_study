0e6c0092ba71fb57f5be1cb0043a81c0a37a88c1
chjcode <107929019+chjcode@users.noreply.github.com>
2024-05-15 23:01:14 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,22 @@
+# 2606 바이러스
+
+def dfs(x):
+    visited[x] = True
+    
+    for node in graph[x]:
+        if not visited[node]:
+            dfs(node)
+    return
+
+n = int(input())  # 컴퓨터 수
+link = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+for _ in range(link):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(1)
+
+print(visited.count(True)-1)
