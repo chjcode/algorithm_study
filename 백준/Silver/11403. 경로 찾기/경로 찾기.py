d6032a87a1007f2c738f45311386ca6711339672
chjcode <107929019+chjcode@users.noreply.github.com>
2024-05-17 18:36:38 +0900

    [Silver I] Title: 경로 찾기, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,12 @@
+
+n = int(input())
+g = [list(map(int,input().split())) for _ in range(n)]
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if g[i][k] == 1 and g[k][j] == 1:
+                g[i][j] = 1
+
+for i in range(n):
+    print(*g[i])
