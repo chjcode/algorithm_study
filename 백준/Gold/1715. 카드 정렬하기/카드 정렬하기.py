e07c6ac9686c477d48a55a34ac285b946f99a1d1
chjcode <107929019+chjcode@users.noreply.github.com>
2024-05-28 10:42:38 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 4096 ms, Memory: 33972 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -0,0 +1,17 @@
+import heapq
+
+n = int(input())
+lst = []
+
+for _ in range(n):
+    heapq.heappush(lst,int(input()))
+
+answer = 0
+while True:
+    if len(lst) < 2:
+        break
+    temp = heapq.heappop(lst) + heapq.heappop(lst)
+    heapq.heappush(lst,temp)
+    answer += temp
+
+print(answer)
