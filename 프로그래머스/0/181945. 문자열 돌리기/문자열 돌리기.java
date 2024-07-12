604294cf0bf5042e8960f63f50aae3f0c66f5121
chjcode <107929019+chjcode@users.noreply.github.com>
2024-07-12 09:03:07 +0900

    [level 0] Title: 문자열 돌리기, Time: 144.45 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+        
+        for (int i = 0; i < s.length();i++){
+            System.out.println(s.charAt(i));
+            
+        }
+        
+    }
+}
