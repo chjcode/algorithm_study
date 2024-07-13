6958ae08094a69aa9371ad0195cd70a09699cbc1
chjcode <107929019+chjcode@users.noreply.github.com>
2024-07-13 16:13:03 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        
+        boolean mode = false;
+        
+        for (int i = 0; i < code.length();i++){
+            if (!mode){
+                if (code.charAt(i) != '1'){
+                    if (i % 2 == 0){
+                        answer += code.charAt(i);
+                    }
+                } else {
+                    mode = true;
+                }
+            } else {
+                if (code.charAt(i) != '1'){
+                    if (i % 2 == 1){
+                        answer += code.charAt(i);
+                    }
+                } else {
+                    mode = false;
+                }
+            }
+        }
+        
+        if (answer.equals("")){
+            answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+}
