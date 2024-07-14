43220fdb90fc85f5f6dd6001a699e52131bf2bd3
chjcode <107929019+chjcode@users.noreply.github.com>
2024-07-14 11:43:56 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+        while (true){
+            if (n == 1){
+                break;
+            }
+            if (n % 2 == 0){
+                n /= 2;
+            } else {
+                n = 3*n+1;
+            }
+            answer.add(n);
+        }
+        
+        return answer;
+    }
+}
