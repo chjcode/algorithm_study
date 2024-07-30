e4df39ae9575c1e8abff71a6d8a6c51bd2ed879a
chjcode <107929019+chjcode@users.noreply.github.com>
2024-07-30 13:24:57 +0900

    [Gold IV] Title: 오큰수, Time: 824 ms, Memory: 148992 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n;
+	static int[] arr;
+	static Deque<Integer> deq;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		deq = new ArrayDeque<>();
+		for (int i = 0; i < n; i++) {
+			while (!deq.isEmpty() && arr[deq.peek()] < arr[i]) {
+				arr[deq.poll()] = arr[i];
+			}
+			
+			deq.push(i);
+		}
+		
+		
+		while (!deq.isEmpty()) {
+			arr[deq.poll()] = -1;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append(arr[i]).append(' ');
+		}
+		
+		System.out.println(sb);
+	}
+
+}
