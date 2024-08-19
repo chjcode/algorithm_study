1ef92c406de97780b166b85e322d9d3c48b844c2
chjcode <107929019+chjcode@users.noreply.github.com>
2024-08-19 18:25:13 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,620 ms, Memory: 24,064 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,14 +1,17 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Solution {
-	
+
 	static int n,l,answer;
 	static List<Hamburger> lst;
+	static int[] check;
 	
 	static class Hamburger{
 		int t;
 		int k;
+		
 		Hamburger(int t, int k) {
 			this.t = t;
 			this.k = k;
@@ -19,9 +22,9 @@ public class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int t = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		for (int tc = 1; tc < t+1; tc++) {
+		for (int tc = 1; tc < T+1; tc++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			l = Integer.parseInt(st.nextToken());
@@ -29,38 +32,75 @@ public class Solution {
 			lst = new ArrayList<>();
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				int taste = Integer.parseInt(st.nextToken());
-				int kal = Integer.parseInt(st.nextToken());
-				
-				lst.add(new Hamburger(taste, kal));
+				lst.add(new Hamburger(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			}
 			
 			answer = 0;
-			
-			for (int i = 0; i < (1<<n); i++) {
-				int totalK = 0;
-				int totalT = 0;
-				boolean flag = true;
-				for (int j = 0; j < n; j++) {
-					if ((i & (1<<j)) != 0) {
-						totalK += lst.get(j).k;
-						totalT += lst.get(j).t;
-					}
-					
-					if (totalK > l) {
-						flag = false;
-						break;
-					}
+			for (int i = 0; i < n; i++) {
+				check = new int[n];
+				for (int j = i ; j < n; j++) {
+					check[j] = 1;
 				}
-				if (flag) {
-					answer = Math.max(answer, totalT);
-				}
+				
+				do {
+					int totalK = 0;
+					int totalT = 0;
+					boolean flag = true;
+					for (int a = 0; a < n; a++) {
+						if (check[a] == 1) {
+							totalK += lst.get(a).k;
+							totalT += lst.get(a).t;
+						}
+						
+						if (totalK > l) {
+							flag = false;
+							continue;
+						}
+						
+						if (flag) {
+							answer = Math.max(answer, totalT);
+						}
+					}
+				}while (nextPermutation(check));
+				
 			}
 			
 			System.out.println("#" + tc + " " + answer);
+			
+		}
+	}
+	
+	private static boolean nextPermutation(int[] arr) {
+		int len = arr.length;
+		int i = len - 1;
+		
+		while (i > 0 && arr[i-1] >= arr[i]) {
+			i--;
 		}
 		
+		if (i == 0) return false;
 		
+		int j = len - 1;
+		while(arr[j] <= arr[i-1]) {
+			j--;
+		}
+		
+		swap(arr,i-1,j);
+		
+		j = len-1;
+		while(i<j) {
+			swap(arr,i,j);
+			i++;
+			j--;
+		}
+		
+		return true;
+	}
+	
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 
 }
