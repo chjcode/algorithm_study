90c1a88dea35ff2cfabd0eaf0a6fae0ef8dae7bd
chjcode <107929019+chjcode@users.noreply.github.com>
2024-08-20 17:31:14 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 105 ms, Memory: 18,656 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -3,53 +3,53 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	
-	static int n,b,hSum,answer;
+
+	static int n, b, hSum, answer;
 	static int[] h;
-	static boolean[] dp;
-	
-	public static void main(String[] args) throws Exception{
+	static List<Integer> lst;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		int t = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc < t+1; tc++) {
+
+		for (int tc = 1; tc < t + 1; tc++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
-			
+
 			h = new int[n];
-			hSum = 0;		// 전체 직원 키의 합
+			hSum = 0; // 전체 직원 키의 합
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) {
 				h[i] = Integer.parseInt(st.nextToken());
 				hSum += h[i];
 			}
 			
-			dp = new boolean[hSum+1];
-			dp[0] = true;
+			lst = new ArrayList<>();
 			answer = Integer.MAX_VALUE;
+			dfs(0,0,0);
 			
-			for(int i = 0; i < n; i++) {
-				for (int j = hSum; j > -1; j--) {
-					if(dp[j]) {
-						int nextIdx = j + h[i];
-						if (nextIdx <= hSum) {
-							dp[nextIdx] = true;
-						}
-						
-						if (nextIdx >= b) {
-							answer = Math.min(answer, nextIdx);
-						}
-					}
-				}
-			}
-			
-			System.out.println("#" + tc + " " + (answer-b));
-			
+			System.out.println("#" + tc + " " + (answer - b));
+		}
+
+	}
+
+	private static void dfs(int start, int cnt, int sum) {
+		
+		if (sum >= b) {
+			answer = Math.min(answer, sum);
+			return;
 		}
 		
-		
+		if (cnt == n) {
+			return;
+		}
+
+		for (int i = start; i < n; i++) {
+			dfs(i+1,cnt+1,sum+h[i]);
+		}
 	}
+
 }
