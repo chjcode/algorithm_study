4202a4b270b2be587906094aedf43eb0d6b6f591
chjcode <107929019+chjcode@users.noreply.github.com>
2024-09-04 10:26:41 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18220 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,29 +3,32 @@ import java.io.*;
 
 public class Main {
 	
-	
-	public static void main(String args[]) throws Exception{
-		
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n+1];
 		
-		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i-1] + 1;
-			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
+		if (n == 1) {
+			System.out.println(0);
+		} else {
+			
+			int[] dp = new int[n+1];
+
+			for (int i = 2; i < n+1; i++) {
+				dp[i] = dp[i-1] + 1;
+				
+				if (i % 2 == 0) {
+					dp[i] = Math.min(dp[i], dp[i/2] + 1);	
+				}
+				if (i % 3 == 0) {
+					dp[i] = Math.min(dp[i], dp[i/3] + 1);
+				}
 			}
-			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
-			}
+			
+			System.out.println(dp[n]);
+
 		}
-		
-//		for (int i = 0; i <= n; i++) {
-//			System.out.print(dp[i] + " ");
-//		}
-//		System.out.println();
-		System.out.println(dp[n]);
-		
 	}
+
 }
