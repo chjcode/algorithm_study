261a32cefe31e6a5310c0d96f2b3b94c6a0c9967
chjcode <107929019+chjcode@users.noreply.github.com>
2024-09-09 10:25:23 +0900

    [Gold V] Title: 두 개의 탑, Time: 164 ms, Memory: 20412 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -6,32 +6,39 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder output = new StringBuilder();
-
-	static int readInt() throws Exception {
+	
+	static int read() throws Exception {
 		if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
 		return Integer.parseInt(st.nextToken());
 	}
 	
-	static long readLong() throws Exception {
-		if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
-		return Long.parseLong(st.nextToken());
-	}
-	
 	public static void main(String[] args) throws Exception {
-		int N = readInt();
-		long[] dist = new long[N];
-		long sumDist = 0L;
+		int N = read();
+		int[] dist = new int[N+1];
+		int sumDist = 0;
 		
 		for(int i = 0 ; i < N ; i++) {
-			long d = readLong();
-			dist[i] = d; sumDist += d;
+			int d = read();
+			dist[i] = d;
+			sumDist += d;
 		}
-		long max = Long.MIN_VALUE;
-		for(int i = 0 ; i < N ; i++) {
-			long left = 0;
-			for(int j = i ; j < N ; j++) {
-				left += dist[j];
-				max = Math.max(max, Math.min(left, sumDist-left));
+		
+		/**
+		 * 투포인터
+		 * 작으면 오른쪽으로 늘려가고 크면 좁혀간다
+		 */
+		
+		int max = Integer.MIN_VALUE;
+		int right = 0, left = 0;
+		int now = dist[left];
+		
+		while(left <= right && right < N) {
+			if(now < sumDist - now) {
+				max = Math.max(max, now);
+				now += dist[++right];
+			} else {
+				max = Math.max(max, sumDist - now);
+				now -= dist[left++];
 			}
 		}
 		System.out.print(max);
