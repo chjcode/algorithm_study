09c22c68fdee70b511ab8dbb45e3bae9a06c31a8
chjcode <107929019+chjcode@users.noreply.github.com>
2024-09-22 23:47:39 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine()).append("??!");
+        System.out.println(sb.toString());
+    }
+}
