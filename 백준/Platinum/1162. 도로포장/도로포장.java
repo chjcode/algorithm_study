bbeeabd3853eda22fe874b3885a0d3b03f553069
chjcode <107929019+chjcode@users.noreply.github.com>
2025-02-26 13:02:54 +0900

    [Platinum V] Title: 도로포장, Time: 640 ms, Memory: 63980 KB -BaekjoonHub

--- 백준/Platinum/1162. 도로포장/도로포장.java
+++ 백준/Platinum/1162. 도로포장/도로포장.java
@@ -1,26 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
     static int n, m, k;
     static List<List<Node>> graph;
-    static long[][] dist;
-    static class Node implements Comparable<Node>{
-        int node;    // 노드
-        long t;      // 시간
-        int cnt;    // 포장 개수
 
-        Node(int node, long t, int cnt) {
+    static class Node implements Comparable<Node> {
+        int node;    // 현재 노드
+        long time;    // 현재 노드까지 오는데 걸린 시간 누적
+        int cnt;    // 현재까지 도로 포장 개수
+        Node(int node, long time, int cnt) {
             this.node = node;
-            this.t = t;
+            this.time = time;
             this.cnt = cnt;
         }
-
+        
         @Override
         public int compareTo(Node n) {
-            return Long.compare(this.t, n.t);
+            return Long.compare(this.time, n.time);
         }
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -28,10 +29,10 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());   // 포장할 도로의 수
+        k = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n+1; i++) {
             graph.add(new ArrayList<>());
         }
 
@@ -39,45 +40,46 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            long t = Long.parseLong(st.nextToken());
+            long time = Long.parseLong(st.nextToken());
 
-            graph.get(u).add(new Node(v,t,0));
-            graph.get(v).add(new Node(u, t, 0));
+            graph.get(u).add(new Node(v,time,0));
+            graph.get(v).add(new Node(u,time,0));
         }
 
-        dist = new long[n+1][k+1];
-        for (int i = 0; i < n + 1; i++) {
-            Arrays.fill(dist[i], Long.MAX_VALUE);
-        }
-
-        dist[1][0] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node(1,0,0));
+        long[][] dist = new long[k+1][n+1];
+        for(int i = 0; i < k+1; i++) {
+            Arrays.fill(dist[i], Long.MAX_VALUE);
+        }
+        dist[0][1] = 0;    // [도로 포장 개수][idx]
 
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
-
-            if (now.t > dist[now.node][now.cnt]) continue;
-
+            Node now = pq.poll();    //현재까지 걸린 시간이 가장 짧은 노드 추출
+            
+            // 현재까지 걸린 시간이 dist에 기록된 최단 시간보다 오래걸리면 continue;
+            if (now.time > dist[now.cnt][now.node]) continue;
+            // 먼저 도착지에 도착한 것이 최소시간이라는 보장이 안되는 이유?
             for (Node next : graph.get(now.node)) {
-                // 도로 포장 o
-                if (now.cnt < k && dist[now.node][now.cnt] < dist[next.node][now.cnt + 1]) {
-                    dist[next.node][now.cnt+1] = dist[now.node][now.cnt];
-                    pq.add(new Node(next.node, dist[next.node][now.cnt+1], now.cnt+1));
+                // 도로 포장 안했을 때 다음 도시까지의 시간이 현재 기록된 시간보다 짧으면
+                if (dist[now.cnt][next.node] > dist[now.cnt][now.node] + next.time) {
+                    dist[now.cnt][next.node] = dist[now.cnt][now.node] + next.time;
+                    pq.add(new Node(next.node, dist[now.cnt][next.node], now.cnt));
                 }
-                //도로 포장 x
-                if (dist[now.node][now.cnt] + next.t < dist[next.node][now.cnt]) {
-                    dist[next.node][now.cnt] = dist[now.node][now.cnt] + next.t;
-                    pq.add(new Node(next.node, dist[next.node][now.cnt], now.cnt));
+                
+                // 도로 포장했을 때 
+                if (now.cnt < k && dist[now.cnt+1][next.node] > dist[now.cnt][now.node]) {
+                    dist[now.cnt+1][next.node] = dist[now.cnt][now.node];
+                    pq.add(new Node(next.node, dist[now.cnt+1][next.node], now.cnt+1));
                 }
             }
+            
         }
-
-        long min = Long.MAX_VALUE;
-        for (int j = 0; j < k+1; j++) {
-
-            min = Math.min(min, dist[n][j]);
+        
+        long minTime = Long.MAX_VALUE;
+        for (int i = 0; i < k+1; i++) {
+            minTime = Math.min(minTime, dist[i][n]);
         }
-        System.out.println(min);
+        System.out.println(minTime);
     }
 }
