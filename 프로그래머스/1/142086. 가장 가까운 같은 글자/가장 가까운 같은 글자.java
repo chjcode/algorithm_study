64e5550f6d125eaf0d37d2465061f285048a48bf
chjcode <107929019+chjcode@users.noreply.github.com>
2025-03-04 08:57:01 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.07 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        int[] alpha = new int[26];
+        Arrays.fill(alpha, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if (alpha[idx] == -1) {
+                answer[i] = -1;
+            } else{
+                answer[i] = i - alpha[idx];
+            }
+            alpha[idx] = i;
+        }
+        
+        return answer;
+    }
+}
