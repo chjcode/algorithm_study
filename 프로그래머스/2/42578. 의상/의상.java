111270808669973aeea641dfcd5456096a5c2757
chjcode <107929019+chjcode@users.noreply.github.com>
2025-03-13 18:08:51 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            if (!map.containsKey(cloth[1])) {
+                map.put(cloth[1], new ArrayList<>());
+            }
+            
+            map.get(cloth[1]).add(cloth[0]);
+        }
+        
+        for(String key : map.keySet()) {
+            answer *= (map.get(key).size() + 1);
+        }
+        
+        return answer - 1;
+    }
+}
