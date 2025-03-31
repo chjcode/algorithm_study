46cb6fef4d8cd90e95549644a14a302859a7e907
chjcode <107929019+chjcode@users.noreply.github.com>
2025-04-01 08:47:45 +0900

    [level 2] Title: 구명보트, Time: 11.23 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,17 +5,22 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(people);
+        
+        int n = people.length;
+        
         int left = 0;
-        int right = people.length-1;
+        int right = n-1;
         
         while (left <= right) {
             if (people[left] + people[right] <= limit) {
                 left++;
             }
+            
             right--;
             answer++;
         }
         
         return answer;
+        
     }
 }
