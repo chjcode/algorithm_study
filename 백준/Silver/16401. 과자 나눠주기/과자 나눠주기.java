c280cdb20a1c95aef5f47a6fdd440e1e1375c1b1
chjcode <107929019+chjcode@users.noreply.github.com>
2025-06-09 10:03:00 +0900

    [Silver II] Title: 과자 나눠주기, Time: 480 ms, Memory: 162048 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -1,52 +1,44 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int m,n,max,answer;
-	static int[] arr;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
-		
-		max = 0;
-		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			max = Math.max(max, arr[i]);
-		}
-		
-		answer = binarySearch(1,max);
-		
-		System.out.println(answer);
-	}
-	
-	private static int binarySearch(int left, int right) {
-		int result = 0;
-		while(left <= right) {
-			int mid = left + (right - left) / 2;
-			int cnt = 0;
-			
-			for (int i = 0; i < arr.length; i++) {
-				cnt += arr[i] / mid;
-			}
-			
-			if (cnt >= m) {
-				result = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		
-		return result;
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        int[] snack = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            snack[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, snack[i]);
+        }
+        
+        int left = 1;
+        int right = max;
+        int answer = 0;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int cnt = 0;
+            
+            for (int i = 0; i < n; i++) {
+                cnt += snack[i] / mid;
+            }
+            
+            if (cnt >= m) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+        
+    }
 }
