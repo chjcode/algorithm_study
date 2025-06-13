69a29c2d195d414842b6672c0a6975c923430eef
chjcode <107929019+chjcode@users.noreply.github.com>
2025-06-13 15:45:58 +0900

    [Gold V] Title: 최소비용 구하기, Time: 412 ms, Memory: 51580 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,82 +3,68 @@ import java.io.*;
 
 public class Main {
 
-	static int n,m;
-	static List<List<Node>> graph;
-	static int[] dist;
-	static boolean[] visited;
-	
-	static class Node implements Comparable<Node>{
-		int idx;
-		int cost;
-		Node(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.cost, o.cost);
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		
-		dist = new int[n+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		
-		visited = new boolean[n+1];
-		
-		graph = new ArrayList<>();
-		for (int i = 0; i < n+1; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			
-			graph.get(u).add(new Node(v,cost));
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		
-		dijkstra(start);
-		
-		System.out.println(dist[end]);
-	}
-	
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start,0));
-		dist[start] = 0;
-		
-		while (!pq.isEmpty()) {
-			Node now = pq.poll();
-			
-			if (visited[now.idx]) continue;
-			
-			visited[now.idx] = true; 
-			for (Node next : graph.get(now.idx)) {
-				
-				// 방문하지 않았고, 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-				if (!visited[next.idx] && (dist[next.idx] > now.cost + next.cost)) {
-					dist[next.idx] = now.cost + next.cost;
-					pq.add(new Node(next.idx, dist[next.idx]));
-				}
-			}
-		}
-		
-		
-	}
-
+    static class City implements Comparable<City> {
+        int v;
+        int cost;
+        City (int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+        
+        @Override
+        public int compareTo(City c) {
+            return this.cost - c.cost;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        List<List<City>> lst = new ArrayList<>();
+        for (int i = 0; i < n+1; i++) {
+            lst.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            lst.get(u).add(new City(v,c));
+        }
+        
+        
+        
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        
+        PriorityQueue<City> pq = new PriorityQueue<>();
+        pq.add(new City(start, 0));
+        
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        
+        while(!pq.isEmpty()) {
+            City now = pq.poll();
+            
+            if (dist[now.v] < now.cost) continue;
+            
+            for (City next : lst.get(now.v)) {
+                if (dist[next.v] > dist[now.v] + next.cost) {
+                    dist[next.v] = dist[now.v] + next.cost;
+                    pq.add(new City(next.v, dist[next.v]));
+                }
+            }
+        }
+        
+        System.out.println(dist[end]);
+        
+    }
 }
