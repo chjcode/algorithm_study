3b2ea98c7133a3057882cdb184248aa35a921c71
chjcode <107929019+chjcode@users.noreply.github.com>
2025-06-28 10:19:03 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2472 ms, Memory: 338556 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -2,34 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static List<Integer> file,file1,file2;
-	static int t,k; 
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		t = Integer.parseInt(br.readLine());
-		
-		for (int tc = 0; tc < t; tc++) {
-			k = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			
-			PriorityQueue<Long> pq = new PriorityQueue<>();
-			for (int i = 0; i < k; i++) {
-				pq.add(Long.parseLong(st.nextToken()));
-			}
-			long sum = 0L;
-			while (pq.size() != 1) {
-				long a = pq.remove();
-				long b= pq.remove();
-				sum += (a+b);
-				pq.add(a + b);
-				
-			}
-			System.out.println(sum);
-		}
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int tc = 0; tc < t; tc++) {
+            int k = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            PriorityQueue<Long> pq = new PriorityQueue<>();
+            for (int i = 0; i < k; i++) {
+                pq.add(Long.parseLong(st.nextToken()));
+            }
+            
+            long answer = 0;
 
+            while(pq.size() != 1) {
+                long a = pq.poll();
+                long b = pq.poll();
+                long sum = a + b;
+                answer += sum;
+                pq.add(sum);
+            }
+            
+            System.out.println(answer);
+        }
+    }
 }
