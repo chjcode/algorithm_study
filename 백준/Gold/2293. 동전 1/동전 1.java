76c4cb089ef775123cdea63affcfc18e5f92e79a
chjcode <107929019+chjcode@users.noreply.github.com>
2025-09-05 10:05:19 +0900

    [Gold IV] Title: 동전 1, Time: 80 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,31 +1,27 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int n,k;
-    static int[] coin, dp;
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        coin = new int[n+1];
-        dp = new int[k+1];
-        dp[0] = 1;  // 아무 동전도 사용하지 않고 0원을 만드는 경우의 수 1
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n+1];
+        int[] dp = new int[k+1];
+        dp[0] = 1;
+        
         for (int i = 1; i < n+1; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
-            for (int j = coin[i]; j <= k; j++) {
-                dp[j] = dp[j] + dp[j - coin[i]];
+            arr[i] = Integer.parseInt(br.readLine());
+            for (int j = arr[i]; j < k+1; j++) {
+                dp[j] = dp[j] + dp[j - arr[i]];
             }
         }
-
+        
+        
         System.out.println(dp[k]);
+        
     }
 }
