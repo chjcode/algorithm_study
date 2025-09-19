68a725434a41bbf52c2d7aa2accc682e1856a45b
chjcode <107929019+chjcode@users.noreply.github.com>
2025-09-19 09:50:43 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -8,30 +8,25 @@ class Main {
         
         int n = Integer.parseInt(br.readLine());
         
-        int[] stairs = new int[n+1];
+        int[] arr = new int[n+1];
         
         for (int i = 1; i < n+1; i++) {
-            stairs[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
         
         int[] dp = new int[n+1];
-        dp[1] = stairs[1];
+        dp[1] = arr[1];
         if (n==1) {
-            dp[1] = stairs[1];
+            dp[1] = arr[1];
         } else if (n == 2) {
-            dp[1] = stairs[1];
-            dp[2] = stairs[2] + stairs[1];
-        } else if (n == 3) {
-            dp[1] = stairs[1];
-            dp[2] = stairs[2] + stairs[1];
-            dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+            dp[1] = arr[1];
+            dp[2] = arr[2] + arr[1];
         } else {
-            dp[1] = stairs[1];
-            dp[2] = stairs[2] + stairs[1];
-            dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+            dp[1] = arr[1];
+            dp[2] = arr[2] + arr[1];
             
-            for (int i = 4; i < n+1; i++) {
-                dp[i] = Math.max(stairs[i] + stairs[i-1] + dp[i-3], stairs[i] + dp[i-2]);
+            for (int i = 3; i < n+1; i++) {
+                dp[i] = Math.max(arr[i] + arr[i-1] + dp[i-3], arr[i] + dp[i-2]);
             }
         }
         
