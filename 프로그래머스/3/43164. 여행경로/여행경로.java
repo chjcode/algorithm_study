dffefc8c1dfb90af8dfefa6e4cfcfbdfa63f4038
chjcode <107929019+chjcode@users.noreply.github.com>
2025-10-17 14:00:24 +0900

    [level 3] Title: 여행경로, Time: 0.67 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,51 +1,59 @@
 import java.util.*;
 
+
 class Solution {
     
+    static String[] arr;
+    static boolean[] visited;
     static int n;
-    static boolean[] used;
-    static List<String> route;
+    static boolean flag = false;
     
     public String[] solution(String[][] tickets) {
-
+        String[] answer = {};
+        
         n = tickets.length;
-        used = new boolean[n];
         
         Arrays.sort(tickets, (a,b) -> {
-            if (a[0].equals(b[0])) {
-                return a[1].compareTo(b[1]);
-            }
-            return a[0].compareTo(b[0]);
+            if (!a[0].equals(b[0])) return a[0].compareTo(b[0]);
+            return a[1].compareTo(b[1]);
         });
         
-        route = new ArrayList<>();
-        route.add("ICN");
+        arr = new String[n+1];
+        visited = new boolean[n];  
         
-        dfs(0, "ICN", tickets);
-        
-        return route.toArray(new String[0]);
-    }
-    
-    private boolean dfs(int depth, String now, String[][] tickets) {
-        if (depth == n) {
-            return true;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (!used[i] && tickets[i][0].equals(now)) {
-                used[i] = true;
-                route.add(tickets[i][1]);
 
-                if (dfs(depth+1,tickets[i][1], tickets)) {
-                    return true;
-                }
+        for(int i = 0; i < n; i++) {
+            if (!visited[i] && tickets[i][0].equals("ICN")) {
 
-                used[i] = false;
-                route.remove(route.size() -1);   
-                
+                arr[0] = tickets[i][0];
+                arr[1] = tickets[i][1];
+                visited[i] = true;
+                dfs(tickets, arr[1], 1);
+                if(flag) return arr;
+                visited[i] = false;
             }
         }
         
-        return false;
+
+        return arr;
+    }
+    
+    private static void dfs(String[][] tickets, String start, int depth) {
+        if (flag) return;
+        if (depth == n) {
+            flag = true;
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if (!visited[i] && start.equals(tickets[i][0])) {
+                arr[depth+1] = tickets[i][1];
+                visited[i] = true;
+                dfs(tickets, tickets[i][1], depth+1);
+                if(flag) return;
+                visited[i] = false;
+            } 
+        }
+
     }
 }
