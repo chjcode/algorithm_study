530509d59c1d7b6137d2b7942bbd12bcf7316ddb
chjcode <107929019+chjcode@users.noreply.github.com>
2025-11-05 02:04:50 +0900

    [Gold V] Title: 퇴사 2, Time: 708 ms, Memory: 331156 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,14 +1,14 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
+public class Main {
     
-    static class Sangdam{
-        int day;
-        int money;
-        Sangdam(int day, int money) {
-            this.day = day;
-            this.money = money;
+    static class Counsel {
+        int t;
+        int p;
+        Counsel(int t, int p) {
+            this.t = t;
+            this.p = p;
         }
     }
     public static void main(String[] args) throws Exception {
@@ -16,31 +16,34 @@ class Main{
         StringTokenizer st;
         
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+2];
-        Sangdam[] sangdam = new Sangdam[n+1];
         
+        Counsel[] counsel = new Counsel[n+2];
+        int maxDate = 0;
         for (int i = 1; i < n+1; i++) {
-            st = new StringTokenizer(br.readLine());    
-            int day = Integer.parseInt(st.nextToken());
-            int money = Integer.parseInt(st.nextToken());
-            sangdam[i] = new Sangdam(day, money);
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            
+            counsel[i] = new Counsel(t,p);
+            maxDate = Math.max(maxDate, t);
         }
         
-        for (int i = 1; i < n+1; i++) {
+        long[] dp = new long[n+2];
+        
+        for (int i = 1; i < n+2; i++) {
             dp[i] = Math.max(dp[i], dp[i-1]);
             
-            if(i+sangdam[i].day <= n+1) {
-                dp[i+sangdam[i].day] = Math.max(dp[i+sangdam[i].day], dp[i] + sangdam[i].money);
+            if (i < n+1) {
+                int end = i + counsel[i].t;
+                if (end < n+2) {
+                    dp[end] = Math.max(dp[end], dp[i] + counsel[i].p);
+                }
             }
             //System.out.println(Arrays.toString(dp));
+            
         }
         
-        dp[n+1] = Math.max(dp[n+1], dp[n]);
-        
         System.out.println(dp[n+1]);
-        
-        
-        
         
     }
 }
