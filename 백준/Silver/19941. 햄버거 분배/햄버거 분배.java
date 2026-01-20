d742b6e185d6a63e5232848bb4480d93f5bdba2d
chjcode <107929019+chjcode@users.noreply.github.com>
2026-01-20 23:15:42 +0900

    [Silver III] Title: 햄버거 분배, Time: 76 ms, Memory: 11908 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -2,26 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, k;
-    static char[] graph;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        graph = new char[n];
+        char[] graph = new char[n];
         String s = br.readLine();
         for (int i = 0; i < n; i++) {
             graph[i] = s.charAt(i);
         }
 
-        // p를 먼저 찾고, p가 먹을 수 있는 햄버거 중
-        // 가장 왼쪽 햄버거 찾기
-        // 먹은 개수만큼 cnt++
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             if (graph[i] == 'P') {
