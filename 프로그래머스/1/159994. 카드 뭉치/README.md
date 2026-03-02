10561f2c41643beda64c80ef8ab527ae5f309b1f
chjcode <107929019+chjcode@users.noreply.github.com>
2026-03-02 21:17:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 75.9 MB, 시간: 0.02 ms
+메모리: 94 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 06일 09:19:26
+2026년 03월 02일 21:17:55
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,24 +1,22 @@
-import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
         
-        int idx1 = 0;
-        int len1 = cards1.length;
-        int idx2 = 0;
-        int len2 = cards2.length;
+        //32
         
-        for (String word : goal) {
-            if (idx1 < len1 && cards1[idx1].equals(word)) {
-                    idx1 += 1;
-            } else if (idx2 < len2 && cards2[idx2].equals(word)) {
-                    idx2 += 1;
+        int idx1 = 0;
+        int idx2 = 0;
+        for (int i = 0; i < goal.length; i++) {
+            if (idx1 < cards1.length && goal[i].equals(cards1[idx1])) {
+                idx1++;
+            } else if (idx2 < cards2.length && goal[i].equals(cards2[idx2])) {
+                idx2++;
             } else {
                 answer = "No";
                 break;
             }
         }
-        
+
         return answer;
     }
 }
