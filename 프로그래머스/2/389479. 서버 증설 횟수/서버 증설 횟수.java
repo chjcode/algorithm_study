7e1052f40084377f2e4412d82cb9b329d2777b72
chjcode <107929019+chjcode@users.noreply.github.com>
2026-03-16 22:16:47 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,33 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] players, int m, int k) {
+        int n = players.length;
+        int[] server = new int[n + k];
         int answer = 0;
-        
-        // m 명 늘어나면 서버 추가 필요
-        // k : 증설한 서버 이용시간
-        int now = 1;
-        
-        int[] server = new int[24];
-        
-        for (int i = 0; i < 24; i++) {
-            int need = 0;
-            if (m > 0) {
-                need = players[i]/m;
-            }
-            if (need > server[i]) {
-                int sub = need - server[i];
-                for (int j = i; j < i+k; j++) {
-                    if (j < 24) {
-                        server[j] += sub;
-                        // System.out.println(Arrays.toString(server));
-                    }
+
+        for (int i = 0; i < n; i++) {
+            int need = players[i] / m;
+
+            if (server[i] < need) {
+                int add = need - server[i];
+                answer += add;
+
+                for (int j = i; j < i + k; j++) {
+                    server[j] += add;
                 }
-                answer += sub;
             }
         }
-        // System.out.println(Arrays.toString(server));
-        
+
         return answer;
     }
 }
