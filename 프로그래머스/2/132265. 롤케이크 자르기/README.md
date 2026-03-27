9d9dfd04dfc43a3e95b63add50d55b44c7232626
chjcode <107929019+chjcode@users.noreply.github.com>
2026-03-27 09:55:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 186.36 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
 
 ### 성능 요약
 
-메모리: 152 MB, 시간: 104.98 ms
+메모리: 161 MB, 시간: 186.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 21:01:01
+2026년 03월 27일 09:55:03
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,40 +4,27 @@ class Solution {
     public int solution(int[] topping) {
         int answer = 0;
         
-        // 동생과 철수가 공평하게 같은 수의 가짓수를 맛볼 수 있도록 자르기?
-        // 공평하게 맛보는 경우의 수
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int t : topping) {
+            map.put(t, map.getOrDefault(t,0)+1);
+        }
         
-        int n = topping.length;
-        
-        int[] c = new int[n];
-        int[] b = new int[n];
-        
-        int idx = 0;
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            if (!set.contains(topping[i])) {
-                set.add(topping[i]);
-                idx++;
+        for (int t : topping) {
+            int cnt = map.get(t)-1;
+            if (cnt == 0) {
+                map.remove(t);
+            } else {
+                map.put(t, cnt);
             }
-            c[i] = idx;
-        }
-        
-        idx = 0;
-        set = new HashSet<>();
-        for (int i = n-1; i > -1; i--) {
-            if (!set.contains(topping[i])) {
-                set.add(topping[i]);
-                idx++;
-            }
-            b[i] = idx;
-        }
-        
-        for (int i = 1; i < n-1; i++) {
-            if (c[i-1] == b[i]) {
+            
+            set.add(t);
+            
+            if (map.size() == set.size()) {
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }
