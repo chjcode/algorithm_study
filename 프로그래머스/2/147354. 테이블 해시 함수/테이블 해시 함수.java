7255f4ee6973a20a9d3a6f22060f84cd7d554932
chjcode <107929019+chjcode@users.noreply.github.com>
2026-04-27 21:17:09 +0900

    [level 2] Title: 테이블 해시 함수, Time: 14.26 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
         
         Arrays.sort(data, (a,b) -> {
             if (a[col-1] == b[col-1]) {
                 return b[0] - a[0];
-            } else {
-                return a[col-1] - b[col-1];
             }
+            return a[col-1] - b[col-1];
         });
         
-        int answer = 0;
-        for (int i = row_begin; i < row_end+1; i++) {
-            int s_i = 0;
-            for (int num : data[i-1]) {
-                s_i += (num % i);
+        for( int i = row_begin-1; i < row_end; i++) {
+            int sum = 0;
+            
+            for (int num : data[i]) {
+                sum += num % (i+1);
             }
             
-            answer ^= s_i;
+            answer ^= sum;
         }
-        
-        
         
         return answer;
     }
