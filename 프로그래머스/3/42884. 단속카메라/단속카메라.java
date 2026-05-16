75686e6528534d9918208814ac7b93a6845aefe8
chjcode <107929019+chjcode@users.noreply.github.com>
2026-05-16 09:33:45 +0900

    [level 3] Title: 단속카메라, Time: 4.25 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,21 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0;
         
-        Arrays.sort(routes, (a,b) -> Integer.compare(a[1],b[1]));
-        
-        int stack = routes[0][1];
-        for (int i = 1; i < routes.length; i++) {
-            if (routes[i][0] <= stack && routes[i][1] >= stack) {
-                
+        Arrays.sort(routes, (a,b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
             } else {
+                return a[0] - b[0];
+            }
+        });
+        
+        //(-20,-15) (-18,-13) (-14,-5), (-5,-3);
+        
+        int end = Integer.MIN_VALUE;
+        for (int[] route : routes) {
+            if (route[0] > end) {
                 answer++;
-                stack = routes[i][1];
-            } 
+                end = route[1];
+            } else {
+                end = Math.min(end, route[1]);
+            }
         }
         
         
         return answer;
+        
     }
 }
